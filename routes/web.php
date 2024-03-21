1893fd38fb4a935eaf9dc674a18b3951f4769964
daxx5020 <dakshmakwana2146@gmail.com>
2024-03-21 17:58:59 +0530

    solved controller path problem

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\http\controllers\DemoController;
+use App\Http\Controllers\DemoController;
 
 /*
 |--------------------------------------------------------------------------
